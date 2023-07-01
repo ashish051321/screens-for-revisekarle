016c1678c81d2110ce7cb8b4b0e2c923679fd48a
ashish051321 <ashish.25jl@gmail.com>
2023-07-01 18:01:48 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  template: './app.component.html',
+  selector: 'my-app',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
