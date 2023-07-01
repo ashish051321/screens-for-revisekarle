b2316893a037070b26ff6f64f04c4bf70731d1ea
ashish051321 <ashish.25jl@gmail.com>
2023-07-01 18:36:10 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, PageNotFoundComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    HeaderComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
